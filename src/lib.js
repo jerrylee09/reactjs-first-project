af333fabe85043d3dfdfab4d1c482f8626c69cb0
jerry <jerry.lee09@yahoo.com>
2017-09-03 17:15:50 -0500

    add style css scss and webpack style loader

--- src/lib.js
+++ src/lib.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import text from './titles.json';
+import './stylesheets/goodbye.scss';
+import './stylesheets/style.css';
+
 
 export const hello = (
     <h1
-        id="title'
-        className='header"
-        style={{background: 'blue', color: 'white'}}
-    >
+        id='title'
+        className='hello'>
         {text.hello}
     </h1>
 );
 
 export const goodbye = (
     <h1
-        id="title"
-        className="header"
-        style={{background: 'black', color: 'white'}}
-    >
+        id='title'
+        className='goodbye'>
         {text.goodbye}
     </h1>
 );
