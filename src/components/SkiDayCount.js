061616fe3752d0b32042dd226bd68ef69a00eea8
jerry <jerry.lee09@yahoo.com>
2017-09-04 12:29:03 -0500

    refractor react  class to es6

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,41 +1,36 @@
-import React from 'react'
+import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
 
 
-export const SkiDayCount = React.createClass({
+export class SkiDayCount extends Component{
 
     percentToDecimal(decimal) {
         return ((decimal * 100) + '%')
-    },
+    }
     calcGoalProgress(total, goal) {
         return this.percentToDecimal(total/goal)
-    },
+    }
     render() {
-        const total = this.props.total;
-        const power = this.props.powder;
-        const backcountry = this.props.backcountry;
-        const goal = this.props.goal;
-        const totalCal = this.calcGoalProgress;
         return (
             <div className="ski-day-count">
                 <div className="total-days">
-                    <span>{total}</span>
+                    <span>{this.props.total}</span>
                     <span> days</span>
                 </div>
                 <div className="powder-days">
-                    <span>{power}</span>
+                    <span>{this.props.powder}</span>
                     <span> days</span>
                 </div>
                 <div className="backcountry-days">
-                    <span>{backcountry}</span>
+                    <span>{this.props.backcountry}</span>
                     <span> hiking day</span>
                 </div>
                 <div >
-                    <span>{totalCal( total, goal)}</span>
+                    <span>{ this.calcGoalProgress( this.props.total, this.props.goal)}</span>
                 </div>
 
             </div>
         )
     }
-});
+};
