7bf0d2c8053be44f953a1bed32e2bb23a7db737a
jerry <jerry.lee09@yahoo.com>
2017-09-09 15:55:32 -0500

    convert element to arrays of objects

--- src/index.js
+++ src/index.js
@@ -1,20 +1,54 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import { hello, goodbye} from './lib'
-import { SkiDayCount } from './components/SkiDayCount.js'
+import { SkiDayList } from './components/SkiDayList'
 // import { BsModal } from './components/BsModal.js'
 
 window.React = React;
 
+// render(
+//     <SkiDayList
+//         total={70}
+//         powder={30}
+//         backcountry={40}
+//         goal={100}
+//     />,
+//     document.getElementById('react-container')
+// );
+
 render(
-    <SkiDayCount
-        total={70}
-        powder={30}
-        backcountry={40}
-        goal={100}
+    <SkiDayList
+        days={
+            [
+                {
+                    resort: "Sqaw Valley",
+                    date: new Date('(9/9/2017'),
+                    powder: true,
+                    backCountry:false
+
+                },
+                {
+                    resort: "Jerry House",
+                    date: new Date('(7/4/2017'),
+                    powder: false,
+                    backCountry:false
+
+                },
+                {
+                    resort: "jerry's Bedroom",
+                    date: new Date('(7/4/2017'),
+                    powder: false,
+                    backCountry:true
+
+                }
+            ]
+        }
     />,
     document.getElementById('react-container')
 );
+
+
+
 
 // render(
 //     <BsModal />,
